f5a3877d436c775f69b59707a7741d1041b3c625
John Janecek <janecektyler@gmail.com>
2022-09-27 16:12:44 -0600

    Add WorldReport0 Plugin event

---
+++ Server.Medius/PluginArgs/OnWorldReport0Args.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RT.Models;
+using Server.Medius.Models;
+
+namespace Server.Medius.PluginArgs
+{
+    public class OnWorldReport0Args
+    {
+        /// <summary>
+        /// Player.
+        /// </summary>
+        public IMediusRequest Request { get; set; }
+
+    }
+}
