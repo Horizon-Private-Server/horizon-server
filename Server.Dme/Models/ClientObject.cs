60edbd04823eaceca6cfbb64f2e3fdbfe1a55e43
Daniel Gerendasy <danielgerendasy@gmail.com>
2023-05-09 17:58:33 -0700

    Fixed timeout for medius version <= 108

--- Server.Dme/Models/ClientObject.cs
+++ Server.Dme/Models/ClientObject.cs
@@ -200,7 +200,8 @@ namespace Server.Dme.Models
             // so instead we'll increment our timeout dates by the client echo
             if (MediusVersion <= 108)
             {
-                UtcLastServerEchoSent = Utils.GetHighPrecisionUtcTime().AddSeconds(-1);
+                // reply must be before sent for the timeout to work
+                UtcLastServerEchoSent = Utils.GetHighPrecisionUtcTime().AddSeconds(1);
                 UtcLastServerEchoReply = Utils.GetHighPrecisionUtcTime();
             }
         }
